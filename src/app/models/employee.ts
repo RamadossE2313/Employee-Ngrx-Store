d817a2e84cc762d63ad0b1d17da675bba61985ef
RamadossElumalai <Ramadossrohit2264@gmail.com>
2024-06-22 16:45:54 +0530

    initial commit

---
+++ src/app/models/employee.ts
@@ -0,0 +1,14 @@
+export class Employee {
+    id: number;
+    name: string;
+    salary: number;
+    gender: string;
+  
+    constructor(id: number, name: string, salary: number, gender: string) {
+      this.id = id;
+      this.name = name;
+      this.salary = salary;
+      this.gender = gender;
+    }
+  }
+  
